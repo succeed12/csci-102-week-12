32288bebc805b0eabf3d45ad77035546444e96bc
succeed12 <bethe1.2succeed@gmail.com>
2019-11-20 16:11:24 -0700

    Created PrintOutput

---
+++ week12-Utility.py
@@ -0,0 +1,9 @@
+# https://github.com/succeed12/csci-102-week-12.git
+#Eshita Mittal
+#CSCI 102 - Section E
+#Python Lab 12 B
+
+#Define Functtoin 1
+def PrintOutput(string_name):
+    print('OUTPUT ', string_name)
+    
