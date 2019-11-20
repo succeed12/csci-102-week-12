61ccebc958caefcc87c91a40fa6bdea99725360f
succeed12 <bethe1.2succeed@gmail.com>
2019-11-20 16:14:48 -0700

    Created LoadFile

--- week12-Utility.py
+++ week12-Utility.py
@@ -6,4 +6,17 @@
 #Define Functtoin 1
 def PrintOutput(string_name):
     print('OUTPUT ', string_name)
+
+#Define Function 2
+def LoadFile(string):
+    with open(string) as file:
+        lines = file.readlines()
+    line_list = []
+    for line in lines:
+        line_list.append(line)
+    print(f'OUTPUT {line_list}')
+
+
+
+
     
